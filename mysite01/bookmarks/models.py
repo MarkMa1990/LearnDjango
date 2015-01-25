4c0b451026a7a1357045a845bf3a276dfbe62a14
MarkMa <mah54746929@qq.com>
2015-01-25 19:58:58 +0800

    modified models User

--- mysite01/bookmarks/models.py
+++ mysite01/bookmarks/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class User(models.Model):
-	username = models.CharField(max_length=30)
-	password = models.IntegerField()
-	email	 = models.EmailField()
+#class User(models.Model):
+#	username = models.CharField(max_length=30)
+#	password = models.IntegerField()
+#	email	 = models.EmailField()
 
 class Link(models.Model):
 	url		 = models.URLField(unique=True)
