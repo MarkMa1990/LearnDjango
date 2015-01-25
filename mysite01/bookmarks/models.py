613490c1f430946bccae992b9bb1f9e285983293
MarkMa <mah54746929@qq.com>
2015-01-25 14:13:01 +0800

    add model User,Link

--- mysite01/bookmarks/models.py
+++ mysite01/bookmarks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	username = models.CharField(max_length=30)
+	password = models.IntegerField()
+	email	 = models.EmailField()
+
+class Link(models.Model):
+	url		 = models.URLField(unique=True)
+	
