8e98aa6821279b6585703612c6d867d05ce6928f
MarkMa <mah54746929@qq.com>
2015-01-25 15:39:28 +0800

    add user_page

--- mysite01/bookmarks/views.py
+++ mysite01/bookmarks/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
+from bookmarks.models import User
 
 def main_page(request):
 	template = get_template('main_page.html')
@@ -11,6 +12,23 @@ def main_page(request):
 		'head_title': 'Django Bookmarks',
 		'page_title': 'Welcome to Django Bookmarks',
 		'page_body' : 'Where you can store and share bookmarks!'
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def user_page(request, username):
+	try:
+		user = User.objects.get(username=username)
+	except:
+		raise Http404('Requseted user not found!')
+
+	bookmarks = user.bookmark_set.all()
+	template = get_template('user_page.html')
+	variables = Context({
+
+		'username' : username,
+		'bookmarks': bookmarks,
+
 		})
 	output = template.render(variables)
 	return HttpResponse(output)
--- mysite01/mysite01/urls.py
+++ mysite01/mysite01/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from bookmarks.views import main_page
+from bookmarks.views import main_page, user_page
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite01.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', main_page),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^user/(\w+)/$', user_page)
 ]
