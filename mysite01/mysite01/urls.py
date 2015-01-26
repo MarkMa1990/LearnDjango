3aa49b3947824a9d957c20abb3ee7bf7ef661215
Mark Ma <mah54746929@qq.com>
2015-01-26 22:24:22 +0800

    using CSS stylesheet

--- mysite01/mysite01/urls.py
+++ mysite01/mysite01/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from bookmarks.views import main_page, user_page, logout_page
+
+import os.path
+site_media = os.path.join(
+	os.path.dirname(__file__), 'site_media'
+	)
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite01.views.home', name='home'),
@@ -9,5 +15,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/(\w+)/$', user_page),
     url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', logout_page)
+    url(r'^logout/$', logout_page),
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': site_media}),
+    
 ]
