7fc2a178fcb4f06310a6e5a4dad0e490b9785ee7
Mark Ma <mah54746929@qq.com>
2015-01-27 00:07:32 +0800

    User Registration stage 1

--- mysite01/bookmarks/views.py
+++ mysite01/bookmarks/views.py
@@ -8,6 +8,7 @@ from bookmarks.models import User
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
+from bookmarks.forms import *
 
 def main_page(request):
 	return render_to_response(
@@ -44,3 +45,25 @@ def user_page(request, username):
 def logout_page(request):
 	logout(request)
 	return HttpResponseRedirect('/')
+
+def register_page(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(
+				username = form.clean_data['username'],
+				password = form.clean_data['password'],
+				email = form.clean_data['email']
+
+				)
+			return HttpResponseRedirect('/')
+		else:
+			form = RegistrationForm()
+		variables = RequestContext(request, {
+			'form' : form
+			})
+		return render_to_response(
+			'registration/register.html',
+			variables
+
+			)
