0b544c21ae2681d899166dd56d93739253f92033
Mark Ma <mah54746929@qq.com>
2015-01-28 18:04:22 +0800

    load search results without reloading the whole page: using ajax

--- mysite01/bookmarks/views.py
+++ mysite01/bookmarks/views.py
@@ -164,4 +164,7 @@ def search_page(request):
 		'show_tags': True,
 		'show_user': True,
 		})
-	return render_to_response('search.html', variables)
+	if request.GET.has_key('ajax'):
+		return render_to_response('bookmark_list.html', variables)
+	else:
+		return render_to_response('search.html', variables)
